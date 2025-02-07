525b62efc23c3b84d4820d4c0cef7d8b50f65be4
Gabriel Loureiro <loureirogabriel672@gmail.com>
2025-02-07 11:17:03 -0300

    Utilizando css para estilização

---
+++ src/components/OutroComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './OutroComponent.module.css'
+
+const OutroComponent = () => {
+  return (
+    <div>
+      <h1 className={style.titulo}>Eu sou o melhor do mundo</h1>
+    </div>
+  )
+}
+
+export default OutroComponent
