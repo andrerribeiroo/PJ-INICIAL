fa6b48f49640b03ec6a5d8ef5efbea6ca738df0f
Andre Ribeiro <168545471+andrerribeiroo@users.noreply.github.com>
2025-02-13 10:51:30 -0300

    .

--- src/components/AulaQuatro.jsx
+++ src/components/AulaQuatro.jsx
@@ -1,24 +1,39 @@
 import React from "react";
 import UseProps from "./UseProps";
+
+import { useState } from "react";
+
 import styles from "./AulaQuatro.module.css";
 
 const AulaQuatro = () => {
+  const imgPadrao =
+    "https://voxnews.com.br/wp-content/uploads/2017/04/unnamed.png";
+  const imgPadrao2 =
+    "https://www.imagensempng.com.br/wp-content/uploads/2023/12/Logo-GTA-6-PNG-Simbolo-Grand-theft-Auto-Vice-City.png";
 
-   const imgPadrao = "https://voxnews.com.br/wp-content/uploads/2017/04/unnamed.png";
-
-   const imgPadrao2 = "https://www.imagensempng.com.br/wp-content/uploads/2023/12/Logo-GTA-6-PNG-Simbolo-Grand-theft-Auto-Vice-City.png";
-
+  //criando os objetos do props
+  const [card] = useState([
+    { imagem: imgPadrao, titulo: "Titulo 1", descricao: "Descrição 1" },
+    { imagem: imgPadrao2, titulo: "Titulo 2", descricao: "Descrição 2" },
+    { imagem: imgPadrao, titulo: "Titulo 3", descricao: "Descrição 3" },
+    { imagem: imgPadrao2, titulo: "Titulo 4", descricao: "Descrição 4" },
+    { imagem: imgPadrao, titulo: "Titulo 5", descricao: "Descrição 5" },
+    { imagem: imgPadrao2, titulo: "Titulo 6", descricao: "Descrição 6" },
+  ]);
 
   return (
-    <div className={styles.container}> 
+    <div className={styles.container}>
       <h1>Cards Incríveis</h1>
-      <div className={styles.cardArea}>
-        <UseProps id="1" imagem={imgPadrao} titulo="Titulo new" descricao="Em breve..."/>
-        <UseProps id="2" imagem={imgPadrao2} titulo="GTA VI" descricao="GAME EM LANÇAMENTO!!"/>
-        <UseProps id="3" imagem={imgPadrao} titulo="Titulo new" descricao="Em breve..."/>
-        <UseProps id="4" imagem={imgPadrao2} titulo="GTA VI" descricao="GAME EM LANÇAMENTO!!"/>
-        <UseProps id="5" imagem={imgPadrao} titulo="Titulo new" descricao="Em breve..."/>
-        <UseProps id="6" imagem={imgPadrao2} titulo="GTA VI" descricao="GAME EM LANÇAMENTO!!"/>
+      {/* passando os objetos para o componente UseProps */}
+      <div className={styles.cards}>
+        {card.map((item, index) => (
+          <UseProps
+            key={index}
+            imagem={item.imagem}
+            titulo={item.titulo}
+            descricao={item.descricao}
+          />
+        ))}
       </div>
     </div>
   );
--- src/components/UseProps.jsx
+++ src/components/UseProps.jsx
@@ -1,18 +1,38 @@
 import React from 'react'
 import style from './UseProps.module.css'
 
-const UseProps = (props) => {
+const UseProps = ({id, imagem, titulo, descricao}) => {
 
-    console.log(props);
+    //console.log(props);
 
     return (
     <div className={style.card}>
-        <img src={props.imagem} alt="" />
-      <h4>{props.titulo}</h4>
-      <p>{props.descricao}</p>
+        <img src={imagem} alt="" />
+      <h4>{titulo}</h4>
+      <p>{descricao}</p>
       <button className={style.btn}>Visualizar</button>
     </div>
   )
 }
 
 export default UseProps;
+
+
+// import React from 'react'
+// import style from './UseProps.module.css'
+
+// const UseProps = (props) => {
+
+//     console.log(props);
+
+//     return (
+//     <div className={style.card}>
+//         <img src={props.imagem} alt="" />
+//       <h4>{props.titulo}</h4>
+//       <p>{props.descricao}</p>
+//       <button className={style.btn}>Visualizar</button>
+//     </div>
+//   )
+// }
+
+// export default UseProps;
